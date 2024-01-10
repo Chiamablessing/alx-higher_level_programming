2b790a33b2147258f898711a246b480397cdaba1
Chiamablessing <chiamablessing@gmail.com>
2024-01-10 16:39:00 +0100

    Print alphabet in uppercase

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-from string import ascii_uppercase; import os;
-os.write(1, bytes(ascii_uppercase + '\n', 'utf-8'))
+from string import ascii_uppercase
+print(*ascii_uppercase, sep='', end='\n')
