44318d0036c0f34ebab1c6bbb72a4dd5036f8172
Chiamablessing <chiamablessing@gmail.com>
2024-04-16 02:12:51 +0100

    Get a State

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
 """
+Prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa.
+"""
+
 import sys
-from sqlalchemy import (create_engine)
-from model_state import Base, State
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+
+def fetchStateByName(username, password, dbname, state_name):
+    """Fetches the State object by name from the database.
+    Attributes:
+        username (str): Username of the user
+        password (str): user password
+        dbname (str): Database to be assessed
+        state_name (str): name of the state
+    """
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(username, password, dbname),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State)
+    for state in states:
+        if state_name == state.name:
+            print("{}".format(state.id))
+            break
+    else:
+        print("Not found")
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],)).first()
-    try:
-        print(instance[0].id)
-    except IndexError:
-        print("Not found")
-        
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name = sys.argv[4]
+
+    fetchStateByName(username, password, dbname, state_name)
+    
