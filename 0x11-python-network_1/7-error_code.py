62b51aa61abc48e51114155efdf9ea3edc3405c5
chiamablessing <chiamablessing@gmail.com>
2024-05-22 03:27:58 -0700

    Fix executables

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,19 +2,17 @@
 """A script that
 - takes in a URL
 - sends a request to the URL
-- displays the body of the response.
+- displays the body of the response 
 """
-
+import sys
 import requests
-from sys import argv
 
-if __name__ == '__main__':
 
-    url = argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-    try:
-        result = requests.get(url)
-        result.raise_for_status()
+    result = requests.get(url)
+    if result.status_code >= 400:
+        print("Error code: {}".format(result.status_code))
+    else:
         print(result.text)
-    except:
-        print('Error code: {}'.format(result.status_code))
